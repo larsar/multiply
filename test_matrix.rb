3c1bb7c01791ac7098e95ef92376e0975ea2bf50
Lars Preben Sørsdahl <lars@preben.no>
2016-04-02 14:06:33 +0200

    WIP add and remove from Matrix

--- test_matrix.rb
+++ test_matrix.rb
@@ -2,9 +2,13 @@ require_relative "matrix"
 require "test/unit"
 
 class TestMatrix < Test::Unit::TestCase
+  @x_from = 2
+  @x_to = 5
+  @y_from = 3
+  @y_to = 8
 
   def setup
-    @matrix = Matrix.new(2,5,3,8)
+    @matrix = Matrix.new(, 5, 3, 8)
   end
 
   def test_matrix_boundary_variabes
@@ -15,9 +19,11 @@ class TestMatrix < Test::Unit::TestCase
   end
 
   def test_matrix_storage_size
-    assert_equal()
+    assert_raise(@matrix.get(1, 3))
+    assert_raise(@matrix.get(2, 2))
+    assert_raise(@matrix.get(3, 9))
+    assert_raise(@matrix.get(2, 2))
   end
 
 
-
-  end
+end
