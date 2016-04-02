974de3f24dcd9b97ecf9b30570a45385699d68fc
Lars Preben Sørsdahl <lars@preben.no>
2016-04-02 12:36:35 +0200

    Started adding matrix tests.

--- test_matrix.rb
+++ test_matrix.rb
@@ -1,2 +1,23 @@
-class TestMatrix
-end
+require_relative "matrix"
+require "test/unit"
+
+class TestMatrix < Test::Unit::TestCase
+
+  def setup
+    @matrix = Matrix.new(2,5,3,8)
+  end
+
+  def test_matrix_boundary_variabes
+    assert_equal(2, @matrix.instance_variable_get(:@x_from))
+    assert_equal(5, @matrix.instance_variable_get(:@x_to))
+    assert_equal(3, @matrix.instance_variable_get(:@y_from))
+    assert_equal(8, @matrix.instance_variable_get(:@y_to))
+  end
+
+  def test_matrix_storage_size
+    assert_equal()
+  end
+
+
+
+  end
